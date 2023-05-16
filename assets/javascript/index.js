ed4d6bd39c0b1122779bbaab88ff25212e3d39ff
ScottE13 <>
2023-05-16 16:33:25 +0000

    Added two player colors and rotates between them

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,9 +1,25 @@
-const space = document.getElementsByClassName('empty-space');
+const space = document.getElementsByClassName("empty-space");
+const turn = document.getElementById("turn");
 
 for (let i = 0; i < 42; i++) {
     space[i].addEventListener("click", addPiece);
+    space[i].classList.add(i);
+    if (i > 34) {
+        space[i].classList.add("starting-option");
+    };
 };
-      
+
 function addPiece() {
-    this.style.backgroundColor = "red";
-  };
+
+    if (turn.textContent == "Red's Turn") {
+        this.classList.add("red");
+        turn.textContent ="Yellow's Turn"
+    } 
+
+    else if (turn.textContent == "Yellow's Turn") {
+        this.classList.add("yellow");
+        turn.textContent ="Red's Turn"
+    };
+};
+   
+
