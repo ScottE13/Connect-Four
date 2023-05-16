40e748b873890a302e3d329cca9572b0a8f6bdf7
ScottE13 <>
2023-05-16 19:07:05 +0000

    Fix to remove function from loop + fixed other minor syntax errors

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,32 +3,34 @@ const turn = document.getElementById("turn");
 
 for (let i = 0; i < 42; i++) {
     space[i].addEventListener("click", addPiece);
-    space[i].classList.add(i);
+    space[i].setAttribute("id", i);
     if (i > 34) {
         space[i].classList.add("starting-option");
-    };
+    }
+}
+
 
 
 function addPiece() {
 
+    var thisSpace = Number(this.id);
+    var belowSpace = thisSpace + 7;
+    
     
     if (turn.textContent == "Red's Turn") {
-        if (this.classList.contains("starting-option") || space[i+7].classList.contains("used") && !this.classList.contains("used"))  {
+        if (this.classList.contains("starting-option") || space[belowSpace].classList.contains("used") && !this.classList.contains("used"))  {
             this.classList.remove("starting-option");
             this.classList.add("red", "used");
-            turn.textContent ="Yellow's Turn"
+            turn.textContent ="Yellow's Turn";
         }
     } 
 
     else if (turn.textContent == "Yellow's Turn") {
-        if (this.classList.contains("starting-option") || space[i+7].classList.contains("used") && !this.classList.contains("used")) {
+        if (this.classList.contains("starting-option") || space[belowSpace].classList.contains("used") && !this.classList.contains("used")) {
             this.classList.remove("starting-option");
             this.classList.add("yellow", "used");
-            turn.textContent ="Red's Turn"
+            turn.textContent ="Red's Turn";
         }
-    };
-};
-
-};
-   
+    }
+}
 
