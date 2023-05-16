d6ba86af982f387179d3f8c6c60fe11a10db174d
ScottE13 <>
2023-05-16 12:50:27 +0000

    Created initial on click event listener for spaces

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,9 @@
-console.log("Script Loaded")
+const space = document.getElementsByClassName('empty-space');
+
+for (let i = 0; i < 42; i++) {
+    space[i].addEventListener("click", addPiece);
+};
+      
+function addPiece() {
+    this.style.backgroundColor = "red";
+  };
