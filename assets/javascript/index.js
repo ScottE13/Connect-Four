c32ec8785ae9a8d6df9978eb7a890ac7fee4cbcc
ScottE13 <>
2023-05-17 01:31:24 +0000

    created currentPlayer variable to consolidate detectWin function code

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,6 @@
 const space = document.getElementsByClassName("space");
 const turn = document.getElementById("turn");
-
-
+let currentPlayer = 1;
 let gamestate = 
 // 0 = empty, 1 = red, 2 = yellow
 [
@@ -12,7 +11,6 @@ let gamestate =
     0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0
 ];
-
 
 for (let i = 0; i < 42; i++) {
     space[i].addEventListener("click", addPiece);
@@ -30,64 +28,52 @@ function addPiece() {
     
     
     if (turn.textContent == "Red's Turn") {
+        currentPlayer = 1;
         if (this.classList.contains("starting-option") || space[belowSpace].classList.contains("used") && !this.classList.contains("used"))  {
             this.classList.remove("starting-option");
             this.classList.add("red", "used");
-            turn.textContent ="Yellow's Turn";
             gamestate[thisSpace] = 1;
+            turn.textContent ="Yellow's Turn";
         }
     } 
 
     else if (turn.textContent == "Yellow's Turn") {
+        currentPlayer = 2;
         if (this.classList.contains("starting-option") || space[belowSpace].classList.contains("used") && !this.classList.contains("used")) {
             this.classList.remove("starting-option");
             this.classList.add("yellow", "used");
-            turn.textContent ="Red's Turn";
             gamestate[thisSpace] = 2;
+            turn.textContent ="Red's Turn";
         }
     }
     detectWin(gamestate);
 }
 
-
 function detectWin(gamestate) {
     for (let i = 0; i < 42; i++) {
         //HORIZONTAL
-        if(
+        if (
             i % 7 <4 &&
-            gamestate[i] === 1 &&
-            gamestate[i+1] === 1 &&
-            gamestate[i+2] === 1 &&
-            gamestate[i+3] === 1
-            ) {
+            gamestate[i] === currentPlayer &&
+            gamestate[i+1] === currentPlayer &&
+            gamestate[i+2] === currentPlayer &&
+            gamestate[i+3] === currentPlayer
+            ) { if (currentPlayer === 1) {
                 alert("Red is the winner!");
-            } 
-            else if (
-            i % 7 <4 &&
-            gamestate[i] === 2 &&
-            gamestate[i+1] === 2 &&
-            gamestate[i+2] === 2 &&
-            gamestate[i+3] === 2   
-            ) {
+            } else if (currentPlayer === 2) {
                 alert("Yellow is the winner!");
-            }
-
+            }}
+    
          // VERTICAL
-         if(
-            gamestate[i] === 1 &&
-            gamestate[i+7] === 1 &&
-            gamestate[i+14] === 1 &&
-            gamestate[i+21] === 1
-            ) {
+         if (
+            gamestate[i] === currentPlayer &&
+            gamestate[i+7] === currentPlayer &&
+            gamestate[i+14] === currentPlayer &&
+            gamestate[i+21] === currentPlayer
+            ) { if (currentPlayer === 1) {
                 alert("Red is the winner!");
-            } 
-            else if (
-            gamestate[i] === 2 &&
-            gamestate[i+7] === 2 &&
-            gamestate[i+14] === 2 &&
-            gamestate[i+21] === 2   
-            ) {
+            } else if (currentPlayer === 2) {
                 alert("Yellow is the winner!");
-            }
+            }}
     }     
 }
